1976201b8a84f47337f8f045aed4727a233a29df
SGrinstead <seth.grinstead3299@gmail.com>
2023-05-16 14:00:20 -0700

    Added Book constructor and test

---
+++ LibraryTDD.tests/BookTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTDD.tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Book_Constructor_InitializesPropertyValues()
+        {
+            Book book = new Book("The Hobbit", "JRR Tolkien");
+
+            Assert.Equal("The Hobbit", book.Title);
+            Assert.Equal("JRR Tolkien", book.Author);
+            //Assert.Null(book.Patron);
+        }
+
+        [Fact]
+        public void Book_IsCheckedOut_ReturnsIfBookHasAPatronAssigned()
+        {
+            Book book = new Book("The Hobbit", "JRR Tolkien");
+
+            //Assert.False(book.IsCheckedOut());
+        }
+    }
+}
